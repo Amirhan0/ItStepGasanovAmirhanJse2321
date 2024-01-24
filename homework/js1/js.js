057d3a474a9578e63bea40006e93cf23aaff7c70
Amirhan0 <amirgasanov246@gmail.com>
2024-01-25 00:58:55 +0600

    js

---
+++ homework/js1/js.js
@@ -0,0 +1,63 @@
+// let nameUser = prompt('Введите свое имя') - Задание первое, Привет имя
+
+// alert ('Привет,' + nameUser)
+
+// let age = prompt('Введите дату своего рождения.') - Задание второе, дата рождения
+// let year = 2024
+
+// alert(2024 - age)
+
+
+// let radius = +prompt('Введите число') - Задание третье, периметр
+
+// alert(radius + radius + radius + radius)
+
+
+// let circle = +prompt('Введите число') - Задание четвертое, окружность
+
+// alert (circle * circle / 3,14)
+
+
+// let l = +prompt('Введите расстояние') - Задание пятое, скорость
+// let time = +prompt('Введите время')
+
+// alert(l/time + 'км/ч')
+
+
+// const dollarS = +prompt('Введите сумму доллара') - Задание шестое, конвеер
+// const euRo = Number(0.92)
+
+// alert(dollarS * euRo)
+
+
+// let gigabyte = +prompt('Укажите обьем флешки') - Задание седьмое
+
+// let mb = 820
+
+// alert(gigabyte * 1024 / mb)
+
+
+// let money = +prompt('Введите сумму') - задание восьмое
+
+// const snickers = 540
+
+// alert(money - snickers + '' + 'Ваша сдача')
+
+
+
+// const n = +prompt('Введите число') - задание десятое
+
+// alert(['Четное','Нечетное'][n * n % 2])
+
+
+// let num = +prompt('Введите число'); - задание девятое
+// let reversedNum = 0;
+
+// while (num > 0) {
+//     reversedNum = reversedNum * 10 + num % 10;
+//     num = Math.floor(num / 10);
+// }
+
+// alert(reversedNum);
+
+
