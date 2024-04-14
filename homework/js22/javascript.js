1ffb4f9d4812073f41e410098a93ba75b3b21274
Amirhan0 <amirgasanov246@gmail.com>
2024-04-14 19:43:35 +0600

    projectBig

---
+++ homework/js22/javascript.js
@@ -0,0 +1,11 @@
+const reg = document.querySelector(".reg");
+
+reg.addEventListener("click", () => {
+  window.location.href = "/registration/reg.html";
+});
+
+const autenf = document.querySelector(".autenf");
+
+autenf.addEventListener("click", () => {
+  window.location.href = "/autenf/autenf.html";
+});
